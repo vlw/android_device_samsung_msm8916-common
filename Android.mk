bebff5c9934840a2a2323b4e3807ced8ddc09275
vlw <vlwwwwww@gmail.com>
2019-05-13 15:06:20 +0300

    Symlinks: Add more modem links

--- Android.mk
+++ Android.mk
@@ -58,17 +58,20 @@ $(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(KM_SYMLINKS)
 
 MODEM_IMAGES := \
-    modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
-    modem.b06 modem.b07 modem.b10 modem.b12 modem.b13 modem.b15 \
-    modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 modem.b21 \
-    modem.b24 modem.b25 modem.b26 modem.b27 modem.mdt
+    modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 \
+    modem.b05 modem.b06 modem.b07 modem.b08 modem.b09 \
+    modem.b10 modem.b11 modem.b12 modem.b13 modem.b14 \
+    modem.b15 modem.b16 modem.b17 modem.b18 modem.b19 \
+    modem.b20 modem.b21 modem.b22 modem.b23 modem.b24 \
+    modem.b25 modem.b26 modem.b27 modem.b28 modem.mdt \
+    mba.mbn
 
 MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MODEM_IMAGES)))
 $(MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "Modem firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+	$(hide) ln -sf /firmware-modem/image/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(MODEM_SYMLINKS)
 
