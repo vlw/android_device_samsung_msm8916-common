6e7d38c69635ddb9ed677dc6d2d4a3381bce96ca
vlw <vlwwwwww@gmail.com>
2019-06-11 00:12:43 +0300

    msm8916-common: Build Samsung LiveDisplay HIDL

--- msm8916.mk
+++ msm8916.mk
@@ -155,8 +155,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-legacymm \
-    vendor.lineage.livedisplay@2.0-service-sysfs
+    vendor.lineage.livedisplay@2.0-service.samsung-qcom
 
 # Media
 PRODUCT_PACKAGES += \
