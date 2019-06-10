5c1753ac325d28fb0164b0a31c568acdc115faf6
vlw <vlwwwwww@gmail.com>
2019-06-10 17:47:38 +0300

    msm8916-common: Build binderized sensors

--- msm8916.mk
+++ msm8916.mk
@@ -232,6 +232,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl.samsung8916 \
+    android.hardware.sensors@1.0-service.samsung8916
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
