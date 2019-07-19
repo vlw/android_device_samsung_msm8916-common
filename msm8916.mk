31c175645d7ae9fb9a2fea885b749758e58e8351
vlw <vlwwwwww@gmail.com>
2019-07-19 20:28:17 +0300

    Revert "msm8916-common: enable IMS"
    * Same with prev revert
    
    This reverts commit efc47070b1714738c357cf7261c02dcdbada0b28.

--- msm8916.mk
+++ msm8916.mk
@@ -217,18 +217,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     qti-telephony-common
 
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    libwpa_client
-
 PRODUCT_BOOT_JARS += \
     telephony-ext
-
-
-# Qualcomm broadcast whitelist
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
 
 # Recovery
 PRODUCT_PACKAGES += \
