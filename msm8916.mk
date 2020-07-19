f892c41bbe0e744a24ba8f2f40e356deb4ee0592
vlw <vlwwwwww@gmail.com>
2020-07-19 04:58:35 +0300

    msm8916-common: build SU
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -247,6 +247,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/_hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/_hals.conf
 
+# SU
+PRODUCT_PACKAGES += \
+    su
+
 # Shims
 PRODUCT_PACKAGES += \
     libshim_binder
