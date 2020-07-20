e6827932647d7745d6c560d8dd031a3ac72e2189
vlw <vlwwwwww@gmail.com>
2020-07-20 03:21:43 +0300

    msm8916-common: Move to Samsung binderized light HAL
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -170,6 +170,11 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.samsung
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service.samsung-qcom
