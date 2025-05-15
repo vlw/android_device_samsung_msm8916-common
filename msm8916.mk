b25014490ba1cc6f79e11553b010a64152f2aa99
vlw <vlwwwwww@gmail.com>
2025-05-16 01:28:25 +0300

    msm8974-common: Build 2.1 health HAL
    The devices fail to boot otherwise.
    
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -167,6 +167,11 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
 # Keymaster
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
 PRODUCT_PACKAGES += \
