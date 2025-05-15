5ae2f587bf90f72029098e9a038c7f34a432d67f
vlw <vlwwwwww@gmail.com>
2025-05-16 01:29:23 +0300

    msm8916-common: Enforce vintf manifest override
    * Apparently this is now required on all non full treble devices since
      google added a sleep(1) in libhidl...
    
    Change-Id: I6d8492dd7c56bfdb2ca00f3477634a46fa55b1c0
    
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -172,6 +172,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
+# HIDL
+PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
+
 # Keymaster
 ifneq ($(TARGET_PROVIDES_KEYMASTER),true)
 PRODUCT_PACKAGES += \
