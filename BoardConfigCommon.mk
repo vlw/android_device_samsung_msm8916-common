08a7527f68b720ada0a022550473cb62651d72ae
vlw <vlwwwwww@gmail.com>
2025-11-01 20:53:27 +0300

    msm8916-common: include all fonts
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,10 +130,6 @@ AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
 BOARD_HAVE_QCOM_FM := true
 TARGET_QCOM_NO_FM_FIRMWARE := true
 
-# Fonts
-EXCLUDE_SERIF_FONTS := true
-SMALLER_FONT_FOOTPRINT := true
-
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive loop.max_part=7
