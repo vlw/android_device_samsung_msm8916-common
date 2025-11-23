33f399b2ae7179da05994be1cf4f6e89561ba61a
vlw <vlwwwwww@gmail.com>
2025-11-24 01:05:35 +0300

    msm8916-common: update sepolicy to S
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,8 +192,8 @@ VENDOR_SECURITY_PATCH := 2017-08-01
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += \
-    $(PLATFORM_PATH)/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/private
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
