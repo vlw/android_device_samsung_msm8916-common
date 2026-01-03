1cb9e56cd0c9df5f3cbdfd8baac5c963047eb83c
vlw <vlwwwwww@gmail.com>
2026-01-03 16:19:46 +0300

    msm8916-common: fix vibrator (use AIDL)
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -234,7 +234,7 @@ endif
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
+    android.hardware.power-service.samsung-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
@@ -295,7 +295,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.3-service.samsung
+    android.hardware.vibrator-service.samsung
 
 # Touch (keydisabler)
 PRODUCT_PACKAGES += \
